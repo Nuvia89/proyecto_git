62c54a1add49889f8dab0aeeb66d19f32d5e46bc
Nuvix <a51menrasy@gmail.com>
2026-05-22 23:12:06 -0600

    Agregar archivo

--- utilidades.py
+++ utilidades.py
@@ -1,2 +1,6 @@
 def saludo(nombre):
-    return "Hola " + nombre
+    return "Hola " + nombre# Funcion nueva en progreso
+def exportar_csv(productos):
+    print('Exportando a CSV...')
+    for p in productos:
+        print(f'{p["nombre"]},{p["cantidad"]},{p["precio"]}')
